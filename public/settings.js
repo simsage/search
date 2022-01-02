f3922e6701ecc2c8af101889143cb1d73b1d4b6f
Rock <rock@simsage.co.uk>
2022-01-02 18:23:46 +0000

    updated react scripts to 5.0.0

--- public/settings.js
+++ public/settings.js
@@ -11,7 +11,7 @@ window.ENV = {
     // dark or light theme?
     theme: 'light',
     // the cloud service layer end-point, change "localhost:8080" to ...
-    api_base: 'https://demo-cloud.simsage.ai/api',
+    api_base: 'http://localhost:8080/api',
     // date picker display format
     date_format: 'yyyy/MM',
     // the details of who we are
