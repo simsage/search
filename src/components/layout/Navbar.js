5b9fadae6b65e43d003d6f29b176a903624201e2
Cole Holyoake <coleholyoake@live.com>
2022-01-31 22:25:13 +1300

    improved two screen switching nav styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -74,80 +74,80 @@ export default class Navbar extends Component {
             return <h1>navbar.js: Something went wrong.</h1>;
         }
         return (
-            <div className="row mx-0 px-0 navbar justify-content-start">
+            <div className={(!this.props.showSearchBar ? "border-bottom-0" : "") + " row mx-0 px-0 navbar justify-content-start"}>
                 {this.props.showSearchBar &&
                     <div className={"col-1 ps-4 pe-0 h-100 d-flex justify-content-end"}>
                         <img src="/images/brand/simsage-logo-no-strapline.svg" alt="" className="h-100" onClick={() => this.home() }/>
                     </div>
                 }
-            <div className="col-11 d-flex justify-content-between align-items-center px-4">
-                {this.props.showSearchBar &&
-                <div className="nav-search-container d-flex align-items-center position-relative">
-                    <span className="nav-search-icon ms-2 d-flex align-items-center">
-                        <img src="../images/icon/icon_n-search.svg" alt="search"
-                             onClick={() => {
-                                 if (this.props.onSearch) this.props.onSearch(this.prop.search_text)
-                             }}
-                        />
-                    </span>
-                    <input type="text" className="nav-search-input ps-1 pe-3" id="simsage-search-text"
-                           onChange={(event) => {
-                               if (this.props.onUpdateSearchText) this.props.onUpdateSearchText(event.target.value)
-                           }}
-                           onKeyDown={(event) => this.doSearch(event)}
-                           onBlur={() => {
-                               window.setTimeout(() => this.setState({show_recents: false}), 100)
-                           }}
-                           onClick={() => this.setState({show_recents: true})}
-                           autoComplete={"off"}
-                           value={this.props.search_text}
-                           onFocus={() => this.recentsDropdown()}
-                           placeholder="SimSage Search..."/>
-                        <div
-                            className={((this.state.show_recents && this.props.save_search_list && this.props.save_search_list.length > 0) ? "d-block" : "d-none") + " recents-menu end-0 position-absolute"}>
-                            <ul className="more-list">
-                                {
-                                    this.props.save_search_list && this.props.save_search_list.map((ss, i) => {
-                                        return (
-                                            <li className="more-list-items px-3 py-3 d-flex justify-content-between align-items-center"
-                                                key={i}
-                                                onClick={(e) => this.onSearchFromSuggestion(e, ss.text)}>
-                                                <span className="no-select">{ss.text}</span>
-                                                <img src="../images/icon/icon_rs-close.svg" alt="search"
-                                                     title={"remove \"" + ss.text + "\""}
-                                                     onClick={(e) => this.removeSavedSearch(e, ss.text)}
-                                                     className="remove-recent"/>
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
+                <div className={(!this.props.showSearchBar ? " justify-content-end col-12" : "justify-content-between col-11") + " d-flex align-items-center px-4"}>
+                    {this.props.showSearchBar &&
+                    <div className="nav-search-container d-flex align-items-center position-relative">
+                        <span className="nav-search-icon ms-2 d-flex align-items-center">
+                            <img src="../images/icon/icon_n-search.svg" alt="search"
+                                onClick={() => {
+                                    if (this.props.onSearch) this.props.onSearch(this.prop.search_text)
+                                }}
+                            />
+                        </span>
+                        <input type="text" className="nav-search-input ps-1 pe-3" id="simsage-search-text"
+                            onChange={(event) => {
+                                if (this.props.onUpdateSearchText) this.props.onUpdateSearchText(event.target.value)
+                            }}
+                            onKeyDown={(event) => this.doSearch(event)}
+                            onBlur={() => {
+                                window.setTimeout(() => this.setState({show_recents: false}), 100)
+                            }}
+                            onClick={() => this.setState({show_recents: true})}
+                            autoComplete={"off"}
+                            value={this.props.search_text}
+                            onFocus={() => this.recentsDropdown()}
+                            placeholder="SimSage Search..."/>
+                            <div
+                                className={((this.state.show_recents && this.props.save_search_list && this.props.save_search_list.length > 0) ? "d-block" : "d-none") + " recents-menu end-0 position-absolute"}>
+                                <ul className="more-list">
+                                    {
+                                        this.props.save_search_list && this.props.save_search_list.map((ss, i) => {
+                                            return (
+                                                <li className="more-list-items px-3 py-3 d-flex justify-content-between align-items-center"
+                                                    key={i}
+                                                    onClick={(e) => this.onSearchFromSuggestion(e, ss.text)}>
+                                                    <span className="no-select">{ss.text}</span>
+                                                    <img src="../images/icon/icon_rs-close.svg" alt="search"
+                                                        title={"remove \"" + ss.text + "\""}
+                                                        onClick={(e) => this.removeSavedSearch(e, ss.text)}
+                                                        className="remove-recent"/>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    }
+                    <div className="d-flex align-items-center">
+                        <div className="d-none d-lg-flex flex-column text-end me-3">
+                            <p className="org-name mb-0 small">{this.props.organisation ? this.props.organisation.name : ""}</p>
+                            <p className="user-name mb-0">{this.getFullUsername()}</p>
+                        </div>
+                        {/* <div className="notifications me-2">
+                            <div className="indicator"></div>
+                            <button className={(this.props.isNotificationsDropdown ? "active" : "") + " btn nav-btn"}
+                                onClick={(e) => this.toggleNotificationsDropdown(e)}>
+                                <img src="../images/icon/icon_n-notification.svg" alt="" className={this.props.isNotificationsDropdown ? "d-none" : ""} />
+                                <img src="../images/icon/icon_n-notification-active.svg" alt="" className={!this.props.isNotificationsDropdown ? "d-none" : ""} />
+                            </button>
+                        </div> */}
+                        <div className="account" title="this is the sign-out button for now">
+                            <button className={(this.props.isAccountsDropdown ? "active" : "") + " btn nav-btn"}
+                                onClick={(e) => this.toggleAccountsMenu(e)}>
+                                <img src="../images/icon/icon_n-account.svg" alt="" className={this.props.isAccountsDropdown ? "d-none" : ""} />
+                                <img src="../images/icon/icon_n-account-active.svg" alt="" className={!this.props.isAccountsDropdown ? "d-none" : ""} />
+                            </button>
                         </div>
                     </div>
-                }
-                <div className="d-flex align-items-center">
-                    <div className="d-none d-lg-flex flex-column text-end me-3">
-                        <p className="org-name mb-0 small">{this.props.organisation ? this.props.organisation.name : ""}</p>
-                        <p className="user-name mb-0">{this.getFullUsername()}</p>
-                    </div>
-                    {/* <div className="notifications me-2">
-                        <div className="indicator"></div>
-                        <button className={(this.props.isNotificationsDropdown ? "active" : "") + " btn nav-btn"}
-                            onClick={(e) => this.toggleNotificationsDropdown(e)}>
-                            <img src="../images/icon/icon_n-notification.svg" alt="" className={this.props.isNotificationsDropdown ? "d-none" : ""} />
-                            <img src="../images/icon/icon_n-notification-active.svg" alt="" className={!this.props.isNotificationsDropdown ? "d-none" : ""} />
-                        </button>
-                    </div> */}
-                    <div className="account" title="this is the sign-out button for now">
-                        <button className={(this.props.isAccountsDropdown ? "active" : "") + " btn nav-btn"}
-                            onClick={(e) => this.toggleAccountsMenu(e)}>
-                            <img src="../images/icon/icon_n-account.svg" alt="" className={this.props.isAccountsDropdown ? "d-none" : ""} />
-                            <img src="../images/icon/icon_n-account-active.svg" alt="" className={!this.props.isAccountsDropdown ? "d-none" : ""} />
-                        </button>
-                    </div>
-                </div>
 
-            </div>
+                </div>
             </div>
         );
     }
